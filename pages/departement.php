0909a96d8436723782a687d657d6b47af45aa595
Maneva <manevarazo@gmail.com>
2025-06-30 10:06:00 +0300

    test

--- pages/departement.php
+++ pages/departement.php
@@ -8,6 +8,7 @@ require_once("../fonction/bdd.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>departement</title>
 </head>
 <body>
--- pages/index.php
+++ pages/index.php
@@ -8,6 +8,9 @@ require_once("../fonction/bdd.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<link rel="stylesheet" type="text/css" href="style.css">
+        <link href="../bootstrap/css/bootstrap.min.css"rel="stylesheet">
+        <script src="../bootstrap/js/bootstrap.bundle.mini.js"></script>
     <title>Document</title>
 </head>
 <body>
@@ -16,37 +19,47 @@ require_once("../fonction/bdd.php");
             
     </header>
     <main>
-        <section>
+        <div class="container">
+        <div class="row justify-content-md-center">
+        <section class="col-sm-12 col-lg-8 bloc">
             <article>
-                 <table border="2" width= 50% style="border-collapse: collapse" >
-                    <th>numero departement<t/h>
-                    <th>departement</th>
-                    <th>last name</th>
-                    <th>first name </th>
+                 <table class=" table table-dark table-striped centered-table" >
+                     <thead>
+                    <th scope="col">numero departement<t/h>
+                    <th scope="col">departement</th>
+                    <th scope="col">last name</th>
+                    <th scope="col">Date </th>
+                    </thead>
                   <?php 
      $resultat=get_actualMAnager();
      while ($donnees = mysqli_fetch_assoc($resultat)) { ?>
-   
+   <link rel="stylesheet" type="text/css" href="style.css">
                     <tr>
-                    <td> <?php echo $donnees['dept_no']; ?> </td>
+                    <td scope="row"> <?php echo $donnees['dept_no']; ?> </td>
                     <form action="departement.php" method="post">
-                        <td>
+                        <td scope="row">
                             <input type="hidden" name="dept_no" value="<?php echo $donnees['dept_no']; ?>">
                             <button type="submit" style="background: none; border: none; color: blue; text-decoration: underline; cursor: pointer;">
                                 <?php echo $donnees['dept_name']; ?>
                             </button>
                         </td>
                     </form>
-                    <td><?php echo $donnees['last_name']; ?> <?php echo $donnees['first_name']; ?> </td>
-                    <td><?php echo $donnees['from_date']; ?> </td>
+                    <td scope="row"><?php echo $donnees['last_name']; ?> <?php echo $donnees['first_name']; ?> </td>
+                    <td scope="row"><?php echo $donnees['from_date']; ?> </td>
                     </tr>
         <?php }  ?> 
+
 
              </table>
             </article>
         </section>
+     </div>
+     </div>
     </main>
 </body>
 </html>
+
+
+
 
 
